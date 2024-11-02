15972b13277023cd09bec851c9d2ec957200b204
rahmatdeep <rahmatdeep@gmail.com>
2024-11-02 20:51:46 +0530

    web3 week-11 completed

--- web3/week-11/vite-project/src/App.jsx
+++ web3/week-11/vite-project/src/App.jsx
@@ -1,22 +1,87 @@
-import { createPublicClient, http } from "viem";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "./App.css";
-import { mainnet } from "viem/chains";
+import {
+  http,
+  createConfig,
+  WagmiProvider,
+  useConnect,
+  useAccount,
+  useBalance,
+  useSendTransaction,
+} from "wagmi";
+import { mainnet } from "wagmi/chains";
+import { injected } from "wagmi/connectors";
+import { useRef } from "react";
 
-const client = createPublicClient({
-  chain: mainnet,
-  transport: http(),
+const queryClient = new QueryClient();
+
+const config = createConfig({
+  chains: [mainnet],
+  connectors: [injected()],
+  transports: {
+    [mainnet.id]: http(),
+  },
 });
 
-async function getBalance() {
-  const res = await client.getBalance({
-    address: "0x075c299cf3b9FCF7C9fD5272cded21A4688bEeD",
-  });
-  console.log(res);
-}
 function App() {
   return (
     <>
-      <button onClick={getBalance}>get balance</button>
+      <WagmiProvider config={config}>
+        <QueryClientProvider client={queryClient}>
+          <WalletConnector />
+          <MyAddress />
+          <EthSend />
+        </QueryClientProvider>
+      </WagmiProvider>
+    </>
+  );
+}
+
+function WalletConnector() {
+  const { connectors, connect } = useConnect();
+  return (
+    <>
+      {connectors.map((connector) => (
+        <button key={connector.uid} onClick={() => connect({ connector })}>
+          {connector.name}
+        </button>
+      ))}
+    </>
+  );
+}
+
+function MyAddress() {
+  const { address } = useAccount();
+  const balance = useBalance({ address });
+
+  return (
+    <>
+      <br />
+      {address}
+      <br />
+      {balance?.data?.formatted}
+    </>
+  );
+}
+
+function EthSend() {
+  const addressRef = useRef(null);
+
+  const { hash: data, sendTransaction } = useSendTransaction();
+
+  async function sendEth() {
+    sendTransaction({
+      to: addressRef.current.value,
+      value: "10000000000000000",
+    });
+  }
+
+  return (
+    <>
+      <br />
+      <input type="text" placeholder="Address" ref={addressRef} />
+      <br />
+      <button onClick={sendEth}>Send 0.1 ETH</button>
     </>
   );
 }
